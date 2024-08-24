28cd31f92c44005bc67821fdec007917438fe072
operation404 <operation404@gmail.com>
2024-08-24 16:06:35 -0700

    Potentially found the source of a bug I've been dealing with, where when a StringName field is set to null in the editor, the property is instead set to an empty stringname.

--- modules/mono/glue/GodotSharp/GodotSharp/Core/StringName.cs
+++ modules/mono/glue/GodotSharp/GodotSharp/Core/StringName.cs
@@ -54,6 +54,14 @@ namespace Godot
         internal static StringName CreateTakingOwnershipOfDisposableValue(godot_string_name nativeValueToOwn)
             => new StringName(nativeValueToOwn);
 
+        // Fixed version?
+        // Not checking `godot_string_name.IsAllocated`, taking ownership over a null ptr
+        // and creating a new empty stringname.
+        // internal static StringName CreateTakingOwnershipOfDisposableValue(godot_string_name nativeValueToOwn)
+        // {
+        //     return nativeValueToOwn.IsAllocated ? new StringName(nativeValueToOwn) : null;
+        // }
+
         /// <summary>
         /// Constructs an empty <see cref="StringName"/>.
         /// </summary>
